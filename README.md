0428b886e1c753dd12bdf41d3a9f61c7ea456fc2
Matt Hogg <matt.hogg74@hotmail.com>
2023-03-16 16:48:27 +0000

    Initial

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # ColorConsole
 
-A description of this package.
+Returns the Unicode coloring convention for console output. **Note this will not work in the debug console (March 2023).**
+
+## How to use example:
+
+`
+import ColorConsole
+
+print(ColorConsoleCodes.red.foreground(.underline) + "This is underlined an in red text" + ColorConsoleCodes.default.foreground())
+`
--- Sources/ColorConsole/ColorConsole.swift
+++ Sources/ColorConsole/ColorConsole.swift
@@ -1,6 +1,15 @@
-public struct ColorConsole {
-    public private(set) var text = "Hello, World!"
+public enum ConsoleColorCodeOption : Int {
+	case none = 0, bold = 1, underline = 4
+}
 
-    public init() {
-    }
+public enum ConsoleColorCodes : Int {
+	case black = 30, red, green, yellow, blue, magenta, cyan, white, `default` = 39
+	case lightBlack = 90, lightRed, lightGreen, lightYellow, lightBlue, lightMagenta, lightCyan, lightWhite
+	
+	func foreground(_ option : ConsoleColorCodeOption = .none) -> String {
+		return "\u{001B}[\(option.rawValue);\(self.rawValue)m"
+	}
+	func background() -> String {
+		return "\u{001B}[\(self.rawValue + 10)m"
+	}
 }
